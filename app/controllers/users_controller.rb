bf0c817a0e0c8b9c17e4b704412b4239e6b4ed17
Jason Elliot <elliotjjason@gmail.com>
2019-03-26 21:33:03 +0530

    signup created edit is pending

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # session[:user_id] = @user.id
-      flash[:success] = "Welcome to Chat App#{@user.username}"
-      # redirect_to user_path(@user)
+       session[:user_id] = @user.id
+      flash[:success] = "Welcome to Chat App #{@user.username}"
+      redirect_to root_path
     else
-      # render 'new'
+      render 'new'
     end
   end
 
